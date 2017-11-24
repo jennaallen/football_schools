8e4d9a9a8f2c64e6a6da5061472dd22f78060222
Jenna Allen <jallen1006@gmail.com>
2017-11-24 14:26:18 -0700

    text matching updates

--- R/fuzzy_text_matching.R
+++ R/fuzzy_text_matching.R
@@ -7,30 +7,18 @@ unique_school_alias <- data.table(school_alias = unique(school_data_transform[sc
 unique_football_names <- data.table(football_schools = c(as.matrix(football_data_transform[, c("school", "opponent")])))
 unique_football_names <- unique(unique_football_names)
 
-agrep2 <- Vectorize(agrep, vectorize.args = "pattern")
-fuzzy_text_matches <- agrep2(unique_football_names$football_schools, unique_school_names$schools, max.distance = 0, ignore.case = TRUE, value = TRUE)
-fuzzy_text_matches_values <- agrep2(unique_football_names$football_schools, unique_school_names$schools, max.distance = 0, ignore.case = TRUE, value = FALSE)
-
-#no
-agrepl2 <- Vectorize(agrepl, vectorize.args = "pattern")
-fuzzy_text_matches2 <- agrepl2(unique_football_names$football_schools, unique_school_names$schools, max.distance = 0, ignore.case = TRUE)
-
-
-fuzzy_text_matches3 <- stringdist(tolower(unique_football_names$football_schools), tolower(unique_school_names$schools))
-
-table(fuzzy_names$football_schools)
-class(ndf)
-#fuzzy_names <- cbind(football_schools = tolower(unique_football_names$football_schools), schools = tolower(unique_school_names$schools))
-#fuzzy_names <- data.table(football_schools = tolower(unique_football_names$football_schools), schools = tolower(unique_school_names$schools))
-#ndf <- expand.grid(lapply(fuzzy_names, levels)) 
-ndf <- CJ(tolower(unique_football_names$football_schools), tolower(unique_school_names$schools))
-#ndf <- ndf[order(ndf$n1),]
+#use stringdist for fuzzy text matching; run through all possible methods used for distance calculation
+#cross join football names with all school names (227 football names * 2187 school names = 496449 possible combinations)
+fuzzy_text_results_all <- CJ(football_schools = tolower(unique_football_names$football_schools), schools = tolower(unique_school_names$schools))
 method_list <- c("osa", "lv", "dl", "hamming", "lcs", "qgram", "cosine", "jaccard", "jw", "soundex")
 for( i in method_list){
   
-  ndf[,i] <- stringdist(ndf$V1,ndf$V2,method=i)
+  fuzzy_text_results_all[,i] <- stringdist(fuzzy_text_results_all$football_schools, fuzzy_text_results_all$schools, method = i)
   
 }
+exact_text_matches <- fuzzy_text_results_all[fuzzy_text_results_all$cosine == 0]
+close_text_matches <- fuzzy_text_results_all[fuzzy_text_results_all$jw < 0.20]
+
 
 ## Hypothetically assumed threshold to predict the suspicious match cases
 ## Cosine score < 0.20 and qgram score < 10
