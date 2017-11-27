d1764f30a326ad2564670868509e11a07c33a7a9
Jenna Allen <jallen1006@gmail.com>
2017-11-27 13:59:57 -0700

    creating a function

--- R/fuzzy_text_matching.R
+++ R/fuzzy_text_matching.R
@@ -1,20 +1,46 @@
-#get unique school names
-#there are some colleges that have the same name but are in different locations, but for matching school name we only need one instance of the name
-unique_school_names <- data.table(schools = unique(school_data_transform[school_data_transform$school_year_start == 2015 & school_data_transform$school_main_campus_flag == 1, "school_name"]))
-
-#get unique football school names by combining values in the school and opponent columns into one column
-unique_football_names <- data.table(football_schools = c(as.matrix(football_data_transform[, c("school", "opponent")])))
-unique_football_names <- unique(unique_football_names)
-
-#use stringdist for fuzzy text matching; run through all possible methods used for distance calculation
-#cross join football names with all school names (227 football names * 2187 school names = 496449 possible combinations)
-fuzzy_text_results_all <- CJ(football_schools = tolower(unique_football_names$football_schools), schools = tolower(unique_school_names$schools))
-method_list <- c("osa", "lv", "dl", "hamming", "lcs", "qgram", "cosine", "jaccard", "jw", "soundex")
-for( i in method_list){
+fuzzy_text_matching <- function(football_text = NULL, before_after = NULL) {
+  #get unique school names
+  #there are some colleges that have the same name but are in different locations, but for matching school name we only need one instance of the name
+  unique_school_names <- data.table(schools = unique(school_data_transform[school_data_transform$school_year_start == 2015 & school_data_transform$school_main_campus_flag == 1, "school_name"]))
   
-  fuzzy_text_results_all[,i] <- stringdist(fuzzy_text_results_all$football_schools, fuzzy_text_results_all$schools, method = i)
+  #get unique football school names by combining values in the school and opponent columns into one column
+  unique_football_names <- unique(data.table(football_schools = c(as.matrix(football_data_transform[, c("school", "opponent")]))))
   
+  #use stringdist for fuzzy text matching; run through all possible methods used for distance calculation
+  #cross join football names with all school names (227 football names * 2187 school names = 496449 possible combinations)
+  fuzzy_text_results_all <- CJ(football_schools = tolower(unique_football_names$football_schools), schools = tolower(unique_school_names$schools))
+  if(is.null(football_text) & is.null(before_after)) {
+    method_list <- c("osa", "lv", "dl", "hamming", "lcs", "qgram", "cosine", "jaccard", "jw", "soundex")
+    for (i in method_list) {
+    fuzzy_text_results_all[,i] <- stringdist(fuzzy_text_results_all$football_schools, fuzzy_text_results_all$schools, method = i)
+    }
+  } else if (is.null(football_text) & !is.null(before_after)) {
+    stop("Text to concantenate with football name is missing")
+  } else if (!is.null(football_text) & is.null(before_after)) {
+    stop("Please specify if text should be concantenated before or after the football name")
+  } else if (!(before_after %in% c("before", "after"))) {
+    stop("The second function argument must hava a value of before or after")
+  } else if (before_after == "before") {
+    fuzzy_text_results_all$football_schools <- paste(football_text, fuzzy_text_results_all$football_schools)
+    method_list <- c("osa", "lv", "dl", "hamming", "lcs", "qgram", "cosine", "jaccard", "jw", "soundex")
+    for (i in method_list) {
+      fuzzy_text_results_all[,i] <- stringdist(fuzzy_text_results_all$football_schools, fuzzy_text_results_all$schools, method = i)
+    }
+    fuzzy_text_results_all$football_schools <- str_trim(gsub(football_text, "", fuzzy_text_results_all$football_schools), side = "left")
+  } else {
+    fuzzy_text_results_all$football_schools <- paste(fuzzy_text_results_all$football_schools, football_text)
+    method_list <- c("osa", "lv", "dl", "hamming", "lcs", "qgram", "cosine", "jaccard", "jw", "soundex")
+    for (i in method_list) {
+      fuzzy_text_results_all[,i] <- stringdist(fuzzy_text_results_all$football_schools, fuzzy_text_results_all$schools, method = i)
+    }
+    fuzzy_text_results_all$football_schools <- str_trim(gsub(football_text, "", fuzzy_text_results_all$football_schools), side = "right")
+  }
+  return(fuzzy_text_results_all)
 }
+
+fuzzy_text_results_all <- fuzzy_text_matching()
+
+
 
 #filter data based on exact and close matches
 exact_text_matches <- fuzzy_text_results_all[fuzzy_text_results_all$cosine == 0, ]
