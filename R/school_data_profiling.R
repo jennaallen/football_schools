76ad839e64c1310d22f39d4afbb83d086b132495
Jenna Allen <jallen1006@gmail.com>
2018-02-03 16:34:56 -0700

    Rewrote code to align with tidyverse so no longer need these

--- R/school_data_profiling.R
+++
@@ -1,5 +0,0 @@
-school_data_character_lengths <- lapply(school_data_transform, function(x) max(nchar(x), na.rm = TRUE))
-summary(select_if(school_data_transform, function(col) is.integer(col) || is.numeric(col)))
-
-football_data_character_lengths <- lapply(football_data_transform, function(x) max(nchar(x), na.rm = TRUE))
-summary(select_if(football_data_transform, is.integer))
